bf4865c551f9b890879376fcce11976d31577883
Marsel Sultanov <80184639+marselsultanov@users.noreply.github.com>
2021-04-19 17:49:17 +0300

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -1,64 +1,64 @@
 job ("Main") {
-	parameters {
-		choiceParam('Branch', ['main', 'msultanov'])
-		activeChoiceParam('Child') {
-			choiceType('CHECKBOX')
-			groovyScript {
-				script('return ["Child1","Child2","Child3","Child4"]')
-			}
-		}
-	}
-	
-	steps {
-		downstreamParameterized {
-			trigger('$Child') {
-				block {
-					buildStepFailure('FAILURE')
-					failure('FAILURE')
-					unstable('UNSTABLE')
-				}
-				parameters {
-					predefinedProp('Branch', '$Branch')
-				}
-			}
-		}
-	}
+  parameters {
+    choiceParam('Branch', ['main', 'msultanov'])
+    activeChoiceParam('Child') {
+      choiceType('CHECKBOX')
+      groovyScript {
+        script('return ["Child1","Child2","Child3","Child4"]')
+      }
+    }
+  }
+
+  steps {
+    downstreamParameterized {
+      trigger('$Child') {
+        block {
+          buildStepFailure('FAILURE')
+          failure('FAILURE')
+          unstable('UNSTABLE')
+        }
+        parameters {
+          predefinedProp('Branch', '$Branch')
+        }
+      }
+    }
+  }
 }
 
 for (i in (1..4)) {
 job("Child$i") {
-	parameters {
-		activeChoiceParam('Branch') {
-			choiceType('SINGLE_SELECT')
-			groovyScript {
-				script('''("git ls-remote -h https://github.com/marselsultanov/jenkins-dsl").execute().text.readLines().collect{
+  parameters {
+    activeChoiceParam('Branch') {
+      choiceType('SINGLE_SELECT')
+      groovyScript {
+        script('''("git ls-remote -h https://github.com/marselsultanov/jenkins-dsl").execute().text.readLines().collect{
 it.split()[1].replaceAll(\'refs/heads/\', \'\')}.sort()''')
-			}
-		}
-	}
-	
-	scm {
-		git {
-		remote {
-			url('https://github.com/marselsultanov/jenkins-dsl')
-		}
-		branch('$Branch')
-		}
-	}
+      }
+    }
+  }
 
-	steps {
-		shell('''bash script.sh > output.txt
+  scm {
+    git {
+    remote {
+      url('https://github.com/marselsultanov/jenkins-dsl')
+  }
+    branch('$Branch')
+    }
+  }
+
+  steps {
+    shell('''bash script.sh > output.txt
 tar -czf ${Branch}_dsl_script.tar.gz jobs.groovy''')
-	}
-	
-	publishers {
-		archiveArtifacts {
-			pattern('${Branch}_dsl_script.tar.gz')
-			allowEmpty(false)
-			onlyIfSuccessful(false)
-			fingerprint(false)
-			defaultExcludes(true)
-		}
-	}
+  }
+
+  publishers {
+    archiveArtifacts {
+      pattern('${Branch}_dsl_script.tar.gz')
+      allowEmpty(false)
+      onlyIfSuccessful(false)
+      fingerprint(false)
+      defaultExcludes(true)
+    }
+  }
 }
 }
